51eb0b513c3d4ee2f28e5b54cba0c25b49d2026d
Admiral Adama <admiraladama99@gmail.com>
2018-09-20 07:58:39 -0700

    CLARITY - refactored speedtest.php

--- speedtest.php
+++ speedtest.php
@@ -1,67 +1,54 @@
 <?php
 
-const FILE_LETTERS_AND_NUMS = array(
-		'a' => 1,
-		'b' => 2,
-		'c' => 3,
-		'd' => 4,
-		'e' => 5,
-		'f' => 6,
-		'g' => 7,
-		'h' => 8
-);
-const FILE_NUMS_AND_LETTERS = array(
-		1 => 'a',
-		2 => 'b',
-		3 => 'c',
-		4 => 'd',
-		5 => 'e',
-		6 => 'f',
-		7 => 'g',
-		8 => 'h'
-);
+Class Timer {
+	public $start_timestamp;
+	
+	function __construct() {
+		$this->start_timestamp = self::get_current_timestamp();
+	}
+	
+	function get_duration_in_milliseconds() {
+		$current_timestamp = self::get_current_timestamp();
+		
+		$total_time = round(($current_timestamp - $this->start_timestamp), 4);
+		
+		$total_time *= 1000;
+		$total_time = round($total_time);
+		
+		return $total_time;
+	}
+	
+	static function get_current_timestamp() {
+		$time = microtime();
+		$time = explode(' ', $time);
+		$time = $time[1] + $time[0];
+		return $time;
+	}
+}
+
+const TIMES_TO_LOOP = 1000000;
 
 // ********************* LOOP 1 **************************
 
-$time = microtime();
-$time = explode(' ', $time);
-$time = $time[1] + $time[0];
-$start = $time;
+$timer = new Timer();
 
-for ( $i = 1; $i <= 10000; $i++ ) {
-	$a = FILE_NUMS_AND_LETTERS[4];
+for ( $i = 1; $i <= TIMES_TO_LOOP; $i++ ) {
+	$a = array('1' => 'bob and jane', 2 => array('bob', 'jane'));
 }
 
-$time = microtime();
-$time = explode(' ', $time);
-$time = $time[1] + $time[0];
-$finish = $time;
-$total_time = round(($finish - $start), 4);
-
-$total_time *= 1000;
-$total_time = round($total_time);
+$total_time = $timer->get_duration_in_milliseconds();
 
 echo "Loop 1 Time: $total_time ms<br />";
 
 
 // ********************* LOOP 2 **************************
 
-$time = microtime();
-$time = explode(' ', $time);
-$time = $time[1] + $time[0];
-$start = $time;
+$timer = new Timer();
 
-for ( $i = 1; $i <= 10000; $i++ ) {
-	$a = array_search(4, FILE_LETTERS_AND_NUMS);
+for ( $i = 1; $i <= TIMES_TO_LOOP; $i++ ) {
+	$a = ['1' => 'bob and jane', 2 => ['bob', 'jane']];
 }
 
-$time = microtime();
-$time = explode(' ', $time);
-$time = $time[1] + $time[0];
-$finish = $time;
-$total_time = round(($finish - $start), 4);
-
-$total_time *= 1000;
-$total_time = round($total_time);
+$total_time = $timer->get_duration_in_milliseconds();
 
 echo "Loop 2 Time: $total_time ms<br />";
