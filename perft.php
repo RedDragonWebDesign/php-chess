0263e9d75ebcc584ac6e458e1968bf507668f3b2
Admiral Adama <admiraladama99@gmail.com>
2018-10-01 02:10:31 -0700

    BUG - rewrote perft to be recursive, fixes out of memory error

--- perft.php
+++ perft.php
@@ -17,93 +17,108 @@ if ( isset($_GET['fen']) ) {
 }
 
 $fen = $board->export_fen();
-
-const PERFT_DEPTH = 3;
-const COUNT_CHECKS_AND_CHECKMATES = FALSE;
-const DEBUG = 0;
+$perft_depth = isset($_GET['perft_depth']) ? $_GET['perft_depth'] : 2;
+$detail_level = isset($_GET['detail_level']) ? $_GET['detail_level'] : 1;
+$debug = isset($_GET['debug']) ? $_GET['debug'] : 0;
 $data = array();
-$move_trees_generated = 0;
-$legal_moves[0][0] = new ChessMove(NULL, NULL, NULL, NULL, NULL, $board);
+$data['move_trees_generated'] = 0; // Used to get the average move time per tree.
+
+function perft($current_depth, $max_depth, $color_to_move, $board, $data, $detail_level) {
+	$data['move_trees_generated']++;
+	$legal_moves_list = ChessRulebook::get_legal_moves_list($color_to_move, $board, TRUE, TRUE, $detail_level >= 3);
+	
+	if ( ! isset($data[$current_depth]['nodes']) ) {
+		$data[$current_depth]['depth'] = $current_depth;
+		$data[$current_depth]['nodes'] = 0;
+		$data[$current_depth]['captures'] = 0;
+		$data[$current_depth]['en_passants'] = 0;
+		$data[$current_depth]['castles'] = 0;
+		$data[$current_depth]['promotions'] = 0;
+		$data[$current_depth]['checks'] = 0;
+		$data[$current_depth]['checkmates'] = 0;
+	}
+	
+	$data[$current_depth]['nodes'] += count($legal_moves_list);
+	
+	if ( $detail_level >= 2 ) {
+		foreach ( $legal_moves_list as $move ) {
+			$data = get_details($move, $current_depth, $data, $detail_level);
+		}
+	}
+	
+	if ( $current_depth != $max_depth ) {
+		foreach ( $legal_moves_list as $move ) {
+			// Doing this recursively instead of a list of moves for each depth prevents "out of memory" errors.
+			$data = perft($current_depth + 1, $max_depth, $move->board->color_to_move, $move->board, $data, $detail_level);
+		}
+	}
+	
+	return $data;
+}
+
+function get_details($move, $depth, $data, $detail_level) {
+	if ( $move->capture ) {
+		$data[$depth]['captures']++;
+	}
+	
+	if ( $move->en_passant ) {
+		$data[$depth]['en_passants']++;
+	}
+	
+	if ( $move->castling ) {
+		$data[$depth]['castles']++;
+	}
+	
+	if ( $move->promotion_piece_type ) {
+		$data[$depth]['promotions']++;
+	}
+	
+	if ( $detail_level >= 3 ) {
+		if ( $move->check ) {
+			$data[$depth]['checks']++;
+		}
+		
+		if ( $move->checkmate ) {
+			$data[$depth]['checkmates']++;
+		}
+	}
+	
+	return $data;
+}
 
 $time = microtime();
 $time = explode(' ', $time);
 $time = $time[1] + $time[0];
 $start = $time;
 
-for ( $depth = 1; $depth <= PERFT_DEPTH; $depth++ ) {
-	$data[$depth]['depth'] = $depth;
-	$data[$depth]['nodes'] = 0;
-	$data[$depth]['captures'] = 0;
-	$data[$depth]['en_passants'] = 0;
-	$data[$depth]['castles'] = 0;
-	$data[$depth]['promotions'] = 0;
-	if ( COUNT_CHECKS_AND_CHECKMATES ) {
-		$data[$depth]['checks'] = 0;
-		$data[$depth]['checkmates'] = 0;
-	} else {
-		$data[$depth]['checks'] = '';
-		$data[$depth]['checkmates'] = '';
-	}
+$data = perft(1, $perft_depth, $board->color_to_move, $board, $data, $detail_level);
+
+$move_trees_generated = $data['move_trees_generated'];
+unset($data['move_trees_generated']);
+
+if ( $debug ) {
+	$legal_moves_list1 = ChessRulebook::get_legal_moves_list($board->color_to_move, $board, TRUE, TRUE, FALSE);
 	
-	$legal_moves[$depth] = array();
-	foreach ( $legal_moves[$depth - 1] as $move ) {
-		$legal_moves_list = ChessRulebook::get_legal_moves_list($move->board->color_to_move, $move->board, TRUE, TRUE, COUNT_CHECKS_AND_CHECKMATES);
-		$move_trees_generated++;
+	foreach ( $legal_moves_list1 as $move1 ) {
+		$key1 = $move1->starting_square->get_alphanumeric() . '-' . $move1->ending_square->get_alphanumeric();
 		
-		foreach ( $legal_moves_list as $move2 ) {
-			if ( DEBUG ) {
-				if ( $depth == 1 ) {
-					$key3 = $move2->starting_square->get_alphanumeric() . '-' . $move2->ending_square->get_alphanumeric();
-					
-					$debug[$key3] = array(
-						'count' => 0,
-						'fen' => $move2->board->export_fen(),
-					);
-				}
-				
-				if ( $depth == 2 ) {
-					$key3 = $move->starting_square->get_alphanumeric() . '-' . $move->ending_square->get_alphanumeric();
-					
-					$debug[$key3]['count']++;
-				}
-			}
+		$debug_data[$key1] = array(
+			'count' => 0,
+			'fen' => $move1->board->export_fen(),
+		);
+		
+		$legal_moves_list2 = ChessRulebook::get_legal_moves_list($move1->board->color_to_move, $move1->board, TRUE, TRUE, FALSE);
+		
+		foreach ( $legal_moves_list2 as $move2 ) {
+			$key2 = $move2->starting_square->get_alphanumeric() . '-' . $move2->ending_square->get_alphanumeric();
 			
-			$legal_moves[$depth][] = $move2;
-			
-			$data[$depth]['nodes']++;
-			
-			if ( $move2->capture ) {
-				$data[$depth]['captures']++;
-			}
-			
-			if ( $move2->en_passant ) {
-				$data[$depth]['en_passants']++;
-			}
-			
-			if ( $move2->castling ) {
-				$data[$depth]['castles']++;
-			}
-			
-			if ( $move2->promotion_piece_type ) {
-				$data[$depth]['promotions']++;
-			}
-			
-			if ( COUNT_CHECKS_AND_CHECKMATES ) {
-				if ( $move2->check ) {
-					$data[$depth]['checks']++;
-				}
-				
-				if ( $move2->checkmate ) {
-					$data[$depth]['checkmates']++;
-				}
-			}
+			$debug_data[$key1]['count']++;
 		}
 	}
-	unset($legal_moves[$depth - 1]);
 }
-
-if ( DEBUG ) {
-	ksort($debug);
+	
+if ( $debug ) {
+	ksort($debug_data);
 }
 
 require_once('views/perft.html');
